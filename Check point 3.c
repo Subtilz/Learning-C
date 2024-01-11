2c0d8e9a96b664ac61d07b796b3012691207cced
Suppawit <kit.na2457@gmail.com>
2024-01-11 13:36:47 +0700

    new commit

---
+++ Check point 3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    char name[100];
+    int high;
+    long long std_id;
+    float weight;
+
+    printf("Please enter your name :");
+    scanf("%s", &name);
+
+    printf("Please enter your ID :");
+    scanf("%lld", &std_id);
+
+    printf("Please enter your weight(kg) :");
+    scanf("%f", &weight);
+
+    printf("Please enter your hight(cm) :");
+    scanf("%d", &high);
+
+    printf("name : %s ID : %lld\n", name, std_id);
+    printf("weight : %.1f hight %d \n", weight, high);
+    return 0;
+}
---
+++ test.5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char name[100];
+    int salary;
+    printf("Enter your name :");
+    scanf("%s", &name);
+
+    printf("Plese enter your salary :");
+    scanf("%d", &salary);
+
+    printf("%s gets %d\n", name, salary);
+    return 0;
+}
