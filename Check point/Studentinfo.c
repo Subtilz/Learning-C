39d80208e22ea8f52d2fe4bb8bed5a8114355d90
Suppawit <kit.na2457@gmail.com>
2024-01-18 10:25:55 +0700

    add work

---
+++ Check point/Studentinfo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name : Suppawit Komkong\n");
+    printf("Student ID : 166204150028\n");
+    printf("Tel : 0649196041\n");
+    printf("Email : suppawit.k@rmutsv.ac.th\n");
+    return 0;
+}
